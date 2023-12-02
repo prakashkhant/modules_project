a4337df1b895be57299cf2a9f02fc2d3b9d3c5ec
prakashkhant <151862961+Prakash-pc-1@users.noreply.github.com>
2023-12-02 16:46:50 +0530

    add_admin + dashboard changes

---
+++ admintab.php
@@ -0,0 +1,51 @@
+<?php
+include './layouts/top.php';
+?>
+<main>
+    <!-- <div class="container-fluid px-4 "> -->
+    <div class="page-header ">
+        <p>Admin Tab</p>
+    </div>
+
+    <div class="p-1 bg-white shadow-sm d-flex justify-content-around align-items-center rounded w-25 m-3">
+        <div>
+            <p class="fs-5 fw-bold">Total Admins</p>
+            <!-- here php scrip for admil total -->
+            <h3 class="fs-2">3</h3>
+        </div>
+        <i class="fas fa-user-tie fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+    </div>
+
+    <div class="create-admin ">
+        <h1 class="heading mb-4">
+            Create new Admin
+        </h1>
+    <form action="#" method="post" class="addadmin ">
+            <div>
+                <label for="admin_name">
+                    Admin Name :
+                </label>
+                <input type="text" name="tbadminname" />
+
+            </div>
+            <div>
+                <input type="submit" value="Create">
+            </div>
+        </form>
+        
+    </div>
+    <div class="admin-img">
+            <img src="./images/admintab.png" alt="" srcset="">
+        </div>
+    <div class="main-content  p-2">
+
+        <!-- </div> -->
+
+</main>
+
+
+
+
+<?php
+include './layouts/bottom.php';
+?>
--- layouts/top.php
+++ layouts/top.php
@@ -27,9 +27,9 @@ if (!isset($_SESSION['id']) && !isset($_SESSION['username'])) {
     <div class="bg-light bg-gradient" id="sidebar-wrapper">
         <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fas fa-user-secret me-4 "></i>Admin</div>
         <div class="list-group list-group-flush my-3">
-            <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
+            <a href="dashboard.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
                 <i class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
-            <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-users-cog me-2"></i>Admins</a>
+            <a href="admintab.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-users-cog me-2"></i>Admins</a>
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-users me-2"></i>Users</a>
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-edit me-2"></i>Add Itmes</a>
             <a href="addModule.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-sitemap me-2"></i>Add Module</a>
