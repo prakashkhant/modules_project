10d047c0c1e777879d72922ebe88e89d6a36080c
prakash <132680273+prakashkhant@users.noreply.github.com>
2023-12-10 13:14:52 +0530

    index page changed
    new index page add
    thori hi change kiya abhi
    log in aur sign up ki click add ki hai
    bas

---
+++ testPages/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>website index</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="icon" type="image/x-icon" href="./images/website_icon.png">
+  <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-primary bg-gradient p-3">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active " aria-current="page" href="#">Home</a>
+          </li>
+          <!-- 
+          <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+       -->
+        </ul>
+        <form class="d-flex" role="search">
+          <!-- <input class="form-control me-2" type="button" placeholder="Search" aria-label="log in"> -->
+
+          <a href="log_in.php" class="btn btn-success m-1" role="button">log in</a>
+          <a href="sign_up.php" class="btn btn-warning m-1" role="button">Sign up</a>
+rfpoewtprowefi[wit[perwit[perwit[ewi]]]]
+        </form>
+      </div>
+    </div>
+  </nav>
+  <!-- nav bar finshed -->
+
+  <main class="bg-light mh-75 " style="height: 600px; background-color: rgba(0,0,255,0.1);">
+    <div class="container text-center">
+      <!-- <h1>This is main body</h1> -->
+      <img src="./images/website_icon.png" alt="website logo" srcset="">
+    </div>
+
+  </main>
+  <!-- footer statred                 -->
+
+  <!-- <footer class="bg-dark text-light text-center p-2">
+<h6>Copy right by <i>Prakash Khant</i></h6>
+</footer> -->
+
+
+  <!-- ---------------------------------footer section start  -->
+
+  <section class="footer">
+    <img src="image/logo.png" class="logo" />
+
+    <div class="container">
+      <div>
+        <h3>About Us</h3>
+        <p>This project is made by<span> XT9 Developers. </span></p>
+      </div>
+
+      <div>
+        <form action="#" method="post">
+        <h3>Get notify about new post ?</h3>
+          <div class="row g-3">
+           
+            <div class="col-2 ">
+              <input type="email" name="" placeholder="Enter your Email" />
+            </div>
+            <div class="col-1">
+              <input type="submit" value="submit">
+            </div>
+          </div>
+        </form>
+        <ul>
+          <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Contact Us</h3>
+        <span>XT9 Developers</span>
+        <span>+(91) 9727669930</span>
+        <span>xt9developers01@gmail.com</span>
+        <a href="welcome.html"><span>www.xt9developers.in</span></a>
+      </div>
+    </div>
+
+    <p>&copy;2023 Reserved by XT9 Developers</p>
+  </section>
+
+  <!-- ----------------------footer section ends  -->
+
+  <!-- line added by Kishan -->
+</body>
+
+</html>
