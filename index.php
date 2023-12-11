760107b48bdceda4f929c0800589aab7c1900156
prakash <132680273+prakashkhant@users.noreply.github.com>
2023-12-11 09:06:56 +0530

    index page changed

--- index.php
+++ index.php
@@ -35,64 +35,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD
-  <nav class="navbar navbar-expand-lg bg-primary bg-gradient p-3">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active " aria-current="page" href="#">Home</a>
-          </li>
-          <!-- 
-          <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-       -->
-        </ul>
-        <form class="d-flex" role="search">
-          <!-- <input class="form-control me-2" type="button" placeholder="Search" aria-label="log in"> -->
-
-          <a href="log_in.php" class="btn btn-success m-1" role="button">log in</a>
-          <a href="registration.php" class="btn btn-warning m-1" role="button">Sign up</a>
-
-        </form>
-      </div>
-    </div>
-  </nav>
-  <!-- nav bar finshed -->
-
-  <main class="bg-light mh-75 " style="height: 600px; background-color: rgba(0,0,255,0.1);">
-    <div class="container text-center">
-      <!-- <h1>This is main body</h1> -->
-      <img src="./images/website_icon.png" alt="website logo" srcset="">
-    </div>
-
-  </main>
-  <!-- footer statred                 -->
-
-  <!-- <footer class="bg-dark text-light text-center p-2">
-<h6>Copy right by <i>Prakash Khant</i></h6>
-</footer> -->
-=======
->>>>>>> 10d047c0c1e777879d72922ebe88e89d6a36080c
 
 
     <!--  ======================= Start Header Area ============================== -->
@@ -166,10 +108,10 @@
                 <div class="row">
                     <div class="col-lg-6 col-md-12 site-title">
                         <h3 class="title-text">Hey are you want to use your Free time ?</h3>
-                        <h4 class="title-text text-uppercase">Senior Wordpress Developer</h4>
+                        <!-- <h4 class="title-text text-uppercase">Senior Wordpress Developer</h4> -->
                         <div class="site-buttons">
-                            <div class="d-flex flex-row flex-wrap">
-                                <a href="sign_up.php" class="btn button primary-button mr-4 text-uppercase">
+                            <div class="d-flex flex-row flex-wrap mt-4">
+                                <a href="registration.php" class="btn button primary-button mr-4 text-uppercase ">
                                     Sign up</a>
                                 <a href="log_in.php" type="button" class="btn button secondary-button text-uppercase">Log in</a>
                             </div>
@@ -649,7 +591,7 @@
         <div class="container">
             <div class="">
                 <div class="site-logo text-center py-4">
-                    <a href="#"><img src="./images/website_icon.png" alt="logo"></a>
+                    <a href="#"><img src="./images/website_icon.png" width="100px" height="100px" alt="logo"></a>
                 </div>
                 <div class="social text-center">
                     <h5 class="text-uppercase">Follow me</h5>
