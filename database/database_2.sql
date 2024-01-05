73496e1224629c91afcab7a1d95a508771b422ca
prakash <132680273+prakashkhant@users.noreply.github.com>
2024-01-05 18:53:00 +0530

    database changed

--- database/database_2.sql
+++ database/database_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2024 at 12:29 PM
+-- Generation Time: Jan 05, 2024 at 02:15 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
