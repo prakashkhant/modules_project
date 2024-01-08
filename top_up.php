f0c6b5fe8397d0db67a34984ab91c741bf12a46f
prakash <132680273+prakashkhant@users.noreply.github.com>
2024-01-08 18:17:04 +0530

    nothing

--- top_up.php
+++ top_up.php
@@ -1,9 +1,5 @@
 
 <style>
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 20px;
-}
 
 #myBtn {
   display: none;
@@ -12,25 +8,31 @@ body {
   right: 30px;
   z-index: 99;
   font-size: 22px;
+  font-weight: 300;
   border: none;
   outline: none;
-  background-color: orange;
+  background-color:  #9fc4c6;
   border: 2px solid #ccc;
   border-radius: 50%;
-  color: white;
+  color: black;
   cursor: pointer;
-  padding: 15px;
+/* transition: all 3s; */
   /* border-radius: 4px; */
 }
 
 #myBtn:hover {
-  background-color: #555;
+  transform: translateY(-4px);
+}
+#myBtn img{
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
 }
 </style>
 
 
 <button onclick="topFunction()" id="myBtn" title="Go to top">
-<span>&#8607;</span></button>
+<img src="images/top-button.png" alt="" srcset=""></button>
 
 
 
