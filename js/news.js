7b3e0e31a9189b2fa34c4435792b170a09754cff
prakash <132680273+prakashkhant@users.noreply.github.com>
2024-03-12 11:05:20 +0530

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "b9ea549fa4fc4abfb04cc113631e099e";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
